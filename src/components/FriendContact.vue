57b20097191abfc055071cd215ded01c2e842cab
dougjones866 <kibromanx@gmail.com>
2021-06-09 22:21:53 -0700

    First commit

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,65 @@
+<template>
+  <li>
+    <h2>{{ name }} {{ isFavorite ? "(Favorite)" : "" }}</h2>
+    <button @click="toggleFavorite">Toggle Favorite</button>
+    <button @click="toggleDetails">
+      {{ detailsAreVisable ? "Hide" : "Show " }}Details
+    </button>
+    <ul v-if="detailsAreVisable">
+      <li><strong>Phone:</strong> {{ phoneNumber }}</li>
+      <li><strong>Email:</strong> {{ emailAddress }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    emailAddress: {
+      type: String,
+      required: true,
+    },
+    isFavorite: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
+  emits: ["toggle-favorite"],
+  // emits: {
+  //   'toggle-favorite': function(id) {
+  //     if (id) {
+  //       return true;
+  //     } else {
+  //       console.warn('Id is missing!');
+  //       return false;
+  //     }
+  //   },
+  // },
+  data() {
+    return {
+      detailsAreVisable: false,
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisable = !this.detailsAreVisable;
+    },
+    toggleFavorite() {
+      this.$emit("toggle-favorite", this.id);
+    },
+  },
+};
+</script>
